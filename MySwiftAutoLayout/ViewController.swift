6ca12a9c3ad52b6b63fd206dbb3e6702fed5fa8a
Oluwatobi Omotayo <tobitech@ymail.com>
2018-01-31 10:45:50 +0100

    setup the project and actor an imageview and a label unto the screen

--- MySwiftAutoLayout/ViewController.swift
+++ MySwiftAutoLayout/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // let's avoid polluting viewDidLoad()
+    // {} is referred to as closure, or annomymous function
+    let bearImageView: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Joing us today in our fun and games!"
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .center
+        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(bearImageView)
+        view.addSubview(descriptionTextView)
+        
+        setupLayout()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupLayout() {
+        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
     }
-
-
+    
 }
 
