8320bf4d60b183635fc980ee065652bc49344ff0
Oluwatobi Omotayo <tobitech@ymail.com>
2018-02-01 14:19:28 +0100

    make ui adapt to landscape using multipliers... define static variable for app's main colour

--- MySwiftAutoLayout/ViewController.swift
+++ MySwiftAutoLayout/ViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
+    }
+    
+    static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1.0)
+}
+
 class ViewController: UIViewController {
     
     // let's avoid polluting viewDidLoad()
@@ -36,13 +44,60 @@ class ViewController: UIViewController {
         textView.isScrollEnabled = false
         return textView
     }()
+    
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.gray, for: .normal)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.mainPink, for: .normal)
+        return button
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = .mainPink
+        pc.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1.0)
+        return pc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(descriptionTextView)
         
+        setupBottomControls()
+        
         setupLayout()
+    }
+    
+    fileprivate func setupBottomControls() {
+        
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlsStackView.axis = .horizontal
+        bottomControlsStackView.distribution = .fillEqually
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(bottomControlsStackView)
+        
+        NSLayoutConstraint.activate([
+//            previousButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+            ])
     }
     
     private func setupLayout() {
